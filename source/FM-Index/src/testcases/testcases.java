58fb05546774b426397e7c6dba8a30f43005fa54
Marko <valdtepes1473@hotmail.com>
2013-10-21 02:04:20 +0200

    Initial commit

---
+++ source/FM-Index/src/testcases/testcases.java
@@ -0,0 +1,53 @@
+package testcases;
+import junit.framework.Assert;
+import FMIndex.*;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testcases {
+
+	@Test
+	public void BWTTest1() {
+		String input = "#abracadabra";
+		String expectedOutput = "ard#rcaaaabb";
+		
+		StringWrapper inputString = new StringWrapper();
+		inputString.string = input;
+		StringWrapper outputString = new StringWrapper();
+		BWT.performBTW(inputString, outputString);
+		
+		
+		Assert.assertEquals(outputString.string, expectedOutput);
+		
+	}
+	@Test
+	public void BWTTest2() {
+		String input = "#mississippi";
+		String expectedOutput = "ipssm#pissii";
+		
+		StringWrapper inputString = new StringWrapper();
+		inputString.string = input;
+		StringWrapper outputString = new StringWrapper();
+		BWT.performBTW(inputString, outputString);
+		
+		
+		Assert.assertEquals(outputString.string, expectedOutput);
+		
+	}
+	
+	@Test
+	public void BWTTest3() {
+		String input = "#ananas";
+		String expectedOutput = "s#nnaaa";
+		
+		StringWrapper inputString = new StringWrapper();
+		inputString.string = input;
+		StringWrapper outputString = new StringWrapper();
+		BWT.performBTW(inputString, outputString);
+		
+		
+		Assert.assertEquals(outputString.string, expectedOutput);
+		
+	}
+}
