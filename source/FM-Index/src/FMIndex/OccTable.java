c2e0daff65fc8ff96b429658f4d4f2bae0c87e33
Maja Kontrec <maja@kontrec.com>
2013-12-08 18:50:35 +0100

    Update OccTable.java
    Added javadoc!

--- source/FM-Index/src/FMIndex/OccTable.java
+++ source/FM-Index/src/FMIndex/OccTable.java
@@ -1,5 +1,11 @@
 package FMIndex;
 
 public interface OccTable {
+	/**
+	 * Returns character occurence for given character and position in the string.
+	 * @param character - character you want to find occurence for
+	 * @param position - last position to count character occurence
+	 * @return character occurence
+	 */
 	int getCharacterOcurrance(Character character, Integer position);
 }
