0a5139c52c8a3f2ab5c0028f9d694c2f9e2c9c15
Maja Kontrec <maja@kontrec.com>
2013-12-08 18:50:03 +0100

    Update WaveletTree.java
    Added javadoc and comments :)

--- source/FM-Index/src/FMIndex/WaveletTree.java
+++ source/FM-Index/src/FMIndex/WaveletTree.java
@@ -1,9 +1,17 @@
 package FMIndex;
 
+/**
+ * Class that represents a wavelet tree.
+ */
 public class WaveletTree implements OccTable {
 
 	WaveletNode root;
 	
+	/**
+	 * Constructor for WaveletTree.
+	 * @param string - input string that you want to build the wavelet tree of
+	 * @param alphabet - Alphabet object containing alphabet information of the given input string
+	 */
 	public WaveletTree(StringWrapper string, Alphabet alphabet){
 		root = new WaveletNode(string, alphabet);
 	}
